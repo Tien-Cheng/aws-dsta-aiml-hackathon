a27cba19dfafc8c797257becb6494b54a208e672
Tien-Cheng <tiencheng@protonmail.com>
2023-03-14 16:20:15 +0800

    feat(backend): helper function to translate text

---
+++ backend/backend/controllers/translate.py
@@ -0,0 +1,16 @@
+from pydantic import validate_arguments
+
+from backend.dependencies.translate import get_translate_client
+
+
+@validate_arguments
+def translate_text(
+    text: str, source_language: str = "auto", target_language: str = "en"
+) -> str:
+    client = get_translate_client()
+    response = client.translate_text(
+        Text=text,
+        SourceLanguageCode=source_language,
+        TargetLanguageCode=target_language,
+    )
+    return response["TranslatedText"]
