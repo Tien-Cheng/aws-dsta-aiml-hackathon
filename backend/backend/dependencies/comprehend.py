ca8af12e966f17c3a94f005f61ef660934fc7fa2
Tien-Cheng <tiencheng@protonmail.com>
2023-03-15 22:58:40 +0800

    refactor(backend): merge boto clients onto single file

--- backend/backend/dependencies/comprehend.py
+++
@@ -1,8 +0,0 @@
-from functools import lru_cache
-
-import boto3
-
-
-@lru_cache()
-def get_comprehend_client():
-    return boto3.client("comprehend")
--- backend/backend/dependencies/transcribe.py
+++
@@ -1,8 +0,0 @@
-from functools import lru_cache
-
-import boto3
-
-
-@lru_cache()
-def get_transcribe_client():
-    return boto3.client("transcribe")
