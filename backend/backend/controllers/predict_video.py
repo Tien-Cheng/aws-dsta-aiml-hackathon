c4fb7bbe4b28503d001174001000ef66a5c92767
Tien-Cheng <tiencheng@protonmail.com>
2023-03-15 22:38:30 +0800

    refactor(backend): modularize predict_video functionality

--- backend/backend/controllers/predict_video.py
+++ backend/backend/controllers/predict_video.py
@@ -1,26 +1,36 @@
+from time import sleep
+
 import boto3
-import time
+from fastapi import status
+from fastapi.exceptions import HTTPException
 
-def predict_video(vid):
+from backend.dependencies.rekognition import get_rekognition_client
 
-    rekognition=boto3.client('rekognition')
 
-  
-    startTextDetection=rekognition.start_text_detection(Video={'S3Object': {'Bucket': 'bymfdata','Name': vid,}})
-    textJobId = startTextDetection['JobId']
-    getTextDetection = rekognition.get_text_detection(JobId=textJobId)
-
-    while(getTextDetection['JobStatus'] == 'IN_PROGRESS'):
-        time.sleep(2)
-    
-        getTextDetection = rekognition.get_text_detection(JobId=textJobId)
-    
-
-                        
-    response=getTextDetection
-    result=""
-    for textDetection in response['TextDetections']:
-        text=textDetection['TextDetection']["DetectedText"]
-        result+=text
-
+def predict_video(filename: str, bucket: str, timeout: int = 3600) -> str:
+    rekognition = get_rekognition_client()
+    job_details = rekognition.start_text_detection(
+        Video={
+            "S3Object": {
+                "Bucket": bucket,
+                "Name": filename,
+            }
+        }
+    )
+    job_id = job_details["JobId"]
+    detections = rekognition.get_text_detection(JobId=job_id)
+    time_elapsed = 0
+    while detections["JobStatus"] == "IN_PROGRESS":
+        if time_elapsed > timeout:
+            raise HTTPException(
+                status_code=status.HTTP_408_REQUEST_TIMEOUT,
+                detail="Timeout while waiting for video text detection",
+            )
+        sleep(2)
+        time_elapsed += 2
+        detections = rekognition.get_text_detection(JobId=job_id)
+    result = ""
+    for text_det in detections["TextDetections"]:
+        text = text_det["TextDetection"]["DetectedText"]
+        result += text
     return result
--- backend/backend/routers/predict.py
+++ backend/backend/routers/predict.py
@@ -2,7 +2,8 @@ from typing import List
 
 from fastapi import APIRouter, Depends, File, Form, UploadFile
 
-from backend.controllers.predict_social_media import predict_from_social_media_post
+from backend.controllers.predict_social_media import \
+    predict_from_social_media_post
 from backend.controllers.predict_text import predict_text
 from backend.controllers.predict_video import predict_video
 from backend.controllers.upload import upload_file_by_content
@@ -32,5 +33,7 @@ def submit_video(
     file: UploadFile = File(...), settings: Settings = Depends(get_settings)
 ):
     upload_file_by_content(file.file, settings.S3_BUCKET_NAME, file.filename)
-    text=predict_video(file.filename)
+    text = predict_video(
+        file.filename, settings.S3_BUCKET_NAME, settings.VIDEO_REKOGNITION_TIMEOUT
+    )
     return predict_text(text, settings.SEGMENT_CHUNK_SIZE, settings.TEXT_ENDPOINT)
