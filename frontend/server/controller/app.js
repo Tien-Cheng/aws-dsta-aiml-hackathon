7229a2cd190098baedf97c66f9026475d597ed4a
tzhenr <127808928+tzhenr@users.noreply.github.com>
2023-03-15 21:21:37 +0800

    Synced endpoint conventions and port number

--- frontend/server/controller/app.js
+++ frontend/server/controller/app.js
@@ -27,10 +27,207 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /* ENDPOINTS */
-app.post('/posts', upload.single("file"), function(req,res){
+app.post('/text', upload.single("file"), function(req,res){
     console.log(req.body)
 
-    return res.status(200).send([{'description': req.body.description}]);
+    // return res.status(200).send([{'description': req.body.text}]);
+    return res.status(200).send([
+      {
+        "text": "Please do not add nonsense",
+        "classes": [
+          {
+            "Name": "non_hate",
+            "Score": 0.9998065829277039
+          },
+          {
+            "Name": "toxic",
+            "Score": 0.0001722178130876273
+          },
+          {
+            "Name": "obscene",
+            "Score": 0.00009582550410414115
+          }
+        ]
+      },
+      {
+        "text": "to Wikipedia. Such edits are",
+        "classes": [
+          {
+            "Name": "non_hate",
+            "Score": 0.9997995495796204
+          },
+          {
+            "Name": "toxic",
+            "Score": 0.00017764008953236043
+          },
+          {
+            "Name": "obscene",
+            "Score": 0.00009818559919949621
+          }
+        ]
+      },
+      {
+        "text": "considered vandalism and quickly undone.",
+        "classes": [
+          {
+            "Name": "non_hate",
+            "Score": 0.9998452663421631
+          },
+          {
+            "Name": "toxic",
+            "Score": 0.00014578818809241056
+          },
+          {
+            "Name": "obscene",
+            "Score": 0.00007812384137650952
+          }
+        ]
+      },
+      {
+        "text": "If you would like to",
+        "classes": [
+          {
+            "Name": "non_hate",
+            "Score": 0.9991036057472229
+          },
+          {
+            "Name": "toxic",
+            "Score": 0.0007668289472348988
+          },
+          {
+            "Name": "obscene",
+            "Score": 0.0002614073164295405
+          }
+        ]
+      },
+      {
+        "text": "experiment, please use the sandbox",
+        "classes": [
+          {
+            "Name": "non_hate",
+            "Score": 0.9998452663421631
+          },
+          {
+            "Name": "toxic",
+            "Score": 0.00014567076868843287
+          },
+          {
+            "Name": "obscene",
+            "Score": 0.00007835692667867988
+          }
+        ]
+      },
+      {
+        "text": "instead. Thank you.",
+        "classes": [
+          {
+            "Name": "non_hate",
+            "Score": 0.9998413324356079
+          },
+          {
+            "Name": "toxic",
+            "Score": 0.00014589595957659185
+          },
+          {
+            "Name": "obscene",
+            "Score": 0.00008212903048843145
+          }
+        ]
+      }
+    ]);
+});
+
+app.post('/video', upload.single("file"), function(req,res){
+  console.log(req.body)
+
+  // return res.status(200).send([{'description': req.body.text}]);
+  return res.status(200).send([
+    {
+      "text": "Please do not add nonsense",
+      "classes": [
+        {
+          "Name": "non_hate",
+          "Score": 0.9998065829277039
+        },
+        {
+          "Name": "toxic",
+          "Score": 0.0001722178130876273
+        },
+        {
+          "Name": "obscene",
+          "Score": 0.00009582550410414115
+        }
+      ]
+    },
+    {
+      "text": "to Wikipedia. Such edits are",
+      "classes": [
+        {
+          "Name": "non_hate",
+          "Score": 0.9997995495796204
+        },
+        {
+          "Name": "toxic",
+          "Score": 0.00017764008953236043
+        },
+        {
+          "Name": "obscene",
+          "Score": 0.00009818559919949621
+        }
+      ]
+    },
+    {
+      "text": "considered vandalism and quickly undone.",
+      "classes": [
+        {
+          "Name": "non_hate",
+          "Score": 0.9998452663421631
+        },
+        {
+          "Name": "toxic",
+          "Score": 0.00014578818809241056
+        },
+        {
+          "Name": "obscene",
+          "Score": 0.00007812384137650952
+        }
+      ]
+    },
+    {
+      "text": "If you would like to",
+      "classes": [
+        {
+          "Name": "non_hate",
+          "Score": 0.9991036057472229
+        },
+        {
+          "Name": "toxic",
+          "Score": 0.0007668289472348988
+        },
+        {
+          "Name": "obscene",
+          "Score": 0.0002614073164295405
+        }
+      ]
+    },
+    {
+      "text": "experiment, please use the sandbox",
+      "classes": [
+        {
+          "Name": "non_hate",
+          "Score": 0.9998452663421631
+        },
+        {
+          "Name": "toxic",
+          "Score": 0.00014567076868843287
+        },
+        {
+          "Name": "obscene",
+          "Score": 0.00007835692667867988
+        }
+      ]
+    }
+  ]);
 });
 
 // Catch all if none of the above endpoints match
--- frontend/server/server.js
+++ frontend/server/server.js
@@ -2,7 +2,7 @@
 // Run: node server.js
 
 var app = require('./controller/app.js');
-var port=8888;
+var port=3000;
 
 var server = app.listen(port, function () {
     console.log('Web App Hosted at http://localhost:%s',port);
