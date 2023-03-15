c8e11f16bb86aa54d58bcef9d9a05ce6c8e436e3
Tien-Cheng <tiencheng@protonmail.com>
2023-03-15 22:42:50 +0800

    fix(backend): attempt retries if rate limited by api

--- backend/backend/controllers/predict_text.py
+++ backend/backend/controllers/predict_text.py
@@ -1,7 +1,9 @@
 import logging
+from time import sleep
 from json import dumps
 from typing import Dict, List
 
+from botocore.exceptions import ClientError
 from pydantic import validate_arguments
 
 from backend.dependencies.comprehend import get_comprehend_client
@@ -17,11 +19,26 @@ def segment_text(text: str, span: int) -> List[str]:
 
 @validate_arguments
 def predict_text(chunk: str, chunk_size: int, endpoint: str) -> List[Dict]:
+    if len(chunk) == 0:
+        return []
     comprehend = get_comprehend_client()
     segments = segment_text(chunk, chunk_size)
     result = []
+    logger.info("Sending %s segments to Comprehend", len(segments))
     for chunk in segments:
-        response = comprehend.classify_document(Text=chunk, EndpointArn=endpoint)
+        attempts = 0
+        while True:
+            try:
+                response = comprehend.classify_document(Text=chunk, EndpointArn=endpoint)
+                break
+            except ClientError as err:
+                attempts += 1
+                logger.info("Being rate limited by Comprehend")
+                logger.info("Sleeping for 5 seconds")
+                sleep(5)
+                if attempts > 3:
+                    logger.error("Failed to classify text: %s", str(err))
+                    raise err
         result.append({"text": chunk, "classes": response["Labels"]})
     logger.info("Predicted: %s", dumps(result))
     return result
