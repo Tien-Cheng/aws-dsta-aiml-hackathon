84183018ffdbc13a087a3c4aa0a040d264c91972
Tien-Cheng <tiencheng@protonmail.com>
2023-03-15 23:50:30 +0800

    feat(backend): translate text if not in english

--- backend/backend/controllers/predict_text.py
+++ backend/backend/controllers/predict_text.py
@@ -1,14 +1,16 @@
 import logging
-from time import sleep
 from json import dumps
 from typing import Dict, List
 
-from botocore.exceptions import ClientError
 from pydantic import validate_arguments
 
 from backend.dependencies.aws_ml import get_comprehend_client
+from backend.settings import get_settings
+
+from .translate import translate_text
 
 logger = logging.getLogger(__name__)
+settings = get_settings()
 
 
 @validate_arguments
@@ -18,15 +20,29 @@ def segment_text(text: str, span: int) -> List[str]:
 
 
 @validate_arguments
-def predict_text(chunk: str, chunk_size: int, endpoint: str) -> List[Dict]:
-    if len(chunk) == 0:
+def predict_text(text: str, chunk_size: int, endpoint: str) -> List[Dict]:
+    if len(text) == 0:
         return []
     comprehend = get_comprehend_client()
-    segments = segment_text(chunk, chunk_size)
+    # Find language
+    lang = comprehend.detect_dominant_language(Text=text)
+    # Check if english
+    if lang["Languages"][0]["LanguageCode"] != "en":
+        # Translate to english
+        text = translate_text(
+            text=text,
+            source_language=lang["Languages"][0]["LanguageCode"],
+            target_language="en",
+        )
+    segments = segment_text(text, chunk_size)
     result = []
     logger.info("Sending %s segments to Comprehend", len(segments))
     for chunk in segments:
-        response = comprehend.classify_document(Text=chunk, EndpointArn=endpoint)
+        # NOTE: Use to avoid rate limit when developing
+        if settings.DEBUG_MOCK_CLASSIFY:
+            response = {"Labels": [{"Name": "non_hate", "Score": 0.9}]}
+        else: 
+            response = comprehend.classify_document(Text=chunk, EndpointArn=endpoint)
         result.append({"text": chunk, "classes": response["Labels"]})
     logger.info("Predicted: %s", dumps(result))
     return result
