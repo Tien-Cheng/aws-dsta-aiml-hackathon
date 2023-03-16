3c6762c366b94350a22c8f6461dc06bb94a37f73
tzhenr <127808928+tzhenr@users.noreply.github.com>
2023-03-16 15:07:12 +0800

    Delete frontend/server directory

--- frontend/server/server.js
+++
@@ -1,9 +0,0 @@
-// Entry point to Backend Server
-// Run: node server.js
-
-var app = require('./controller/app.js');
-var port=8080;
-
-var server = app.listen(port, function () {
-    console.log('Web App Hosted at http://localhost:%s',port);
-});
