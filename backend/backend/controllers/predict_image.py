6af55f980e831d31295c3963b05285e3742229d5
Tien-Cheng <tiencheng@protonmail.com>
2023-03-16 18:09:20 +0800

    style(backend): format with black and isort

--- backend/backend/controllers/predict_image.py
+++ backend/backend/controllers/predict_image.py
@@ -7,7 +7,7 @@ from uuid import uuid4
 from fastapi import status
 from fastapi.exceptions import HTTPException
 
-from backend.dependencies.aws_ml import (get_rekognition_client)
+from backend.dependencies.aws_ml import get_rekognition_client
 
 logger = logging.getLogger(__name__)
 
@@ -15,8 +15,8 @@ logger = logging.getLogger(__name__)
 def predict_image(filename: str, bucket: str) -> str:
     rekognition = get_rekognition_client()
 
-    #Text Detection
-    detections= rekognition.detect_text(
+    # Text Detection
+    detections = rekognition.detect_text(
         Image={
             "S3Object": {
                 "Bucket": bucket,
@@ -24,9 +24,9 @@ def predict_image(filename: str, bucket: str) -> str:
             }
         }
     )
-    
-    result=""
-    
+
+    result = ""
+
     result += "DETECTED TEXT (OCR): "
     for text_det in detections["TextDetections"]:
         text = text_det["DetectedText"]
@@ -39,8 +39,8 @@ def predict_image(filename: str, bucket: str) -> str:
 
 def moderate_image(filename: str, bucket: str) -> dict:
     rekognition = get_rekognition_client()
-    #Content Moderation
-    moderations= rekognition.detect_moderation_labels(
+    # Content Moderation
+    moderations = rekognition.detect_moderation_labels(
         Image={
             "S3Object": {
                 "Bucket": bucket,
@@ -50,9 +50,10 @@ def moderate_image(filename: str, bucket: str) -> dict:
     )
     results = []
     for label in moderations["ModerationLabels"]:
-        results.append({
-            "Name": label["Name"],
-            "Score": label["Confidence"],
-        })
+        results.append(
+            {
+                "Name": label["Name"],
+                "Score": label["Confidence"],
+            }
+        )
     return results
-
